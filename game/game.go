d9e0af24a008082e12acf1920f482e6b92ba2364
Thomas Fischer <tdf.tomfischer@gmail.com>
2016-04-11 16:34:56 -0500

    Add client-server registration.

--- game/game.go
+++ game/game.go
@@ -1,12 +1,11 @@
 package game
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
 //	"github.com/gragas/jabberwock-server/inventory"
 //	"github.com/gragas/jabberwock-server/entity"
 	"github.com/gragas/jabberwock-lib/consts"
-	"io"
 	"net"
 	"strconv"
 	"time"
@@ -18,13 +17,13 @@ func StartGame(ip string, port int, quiet bool, debug bool, done chan<- string) 
 	bindAndListen(ip, port, ch, quiet, done)
 }
 
-func loop(ch <-chan string, debug bool) {
+func loop(ch chan string, debug bool) {
 	for {
 		startTime := time.Now()
 		
 		select {
 		case msg := <- ch:
-			handleMessage(msg, debug)
+			handleMessage(msg, ch, debug)
 		default:
 			if debug {
 				fmt.Printf("Nothing received!\n")
@@ -41,24 +40,41 @@ func loop(ch <-chan string, debug bool) {
 	}
 }
 
-func handleMessage(msg string, debug bool) {
+func registerClient(ch chan string, debug bool) {
 	if debug {
-		fmt.Printf("%s\n", msg)
+		fmt.Printf("Registering client...\n")
+	}
+	ch <- "SUCCESS_\n"
+}
+
+func handleMessage(msg string, ch chan string, debug bool) {
+	if debug {
+		fmt.Printf("Received msg: %s\n", msg)
+	}
+	switch msg[:8] {
+	case "REGISTER":
+		registerClient(ch, debug)
+	default:
+		fmt.Printf("Unknown command: %s\n", msg)
 	}
 }
 
-func handleConnection(conn net.Conn, ch chan<- string, quiet bool) {
+func handleConnection(conn net.Conn, ch chan string, handled chan int, quiet bool) {
 	if !quiet {
 		fmt.Printf("Accepted connection from %v\n", conn.RemoteAddr())
 	}
-	var buf bytes.Buffer
-	io.Copy(&buf, conn)
-	ch <- buf.String()
+	msg, err := bufio.NewReader(conn).ReadString('\n')
+	if err != nil {
+		fmt.Printf("Bad message from %v\n", conn.RemoteAddr())
+		return
+	}
+	ch <- msg[:len(msg)-1]
+	handled <- 0
 }
 
 func bindAndListen(ip string,
 	port int,
-	ch chan<- string,
+	ch chan string,
 	quiet bool,
 	done chan<- string) {
 	
@@ -73,6 +89,13 @@ func bindAndListen(ip string,
 		if err != nil {
 			panic(err)
 		}
-		go handleConnection(conn, ch, quiet)
+		handled := make(chan int)
+		go handleConnection(conn, ch, handled, quiet)
+		<-handled
+		serverResponse := <-ch
+		if !quiet {
+			fmt.Printf("Writing %s in response\n", serverResponse[:8])
+		}
+		fmt.Fprintf(conn, serverResponse)
 	}
 }
